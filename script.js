b8bc3f384abbff8009b1529a3fb51fb600eb9e07
Zaoui Amine <aminedazaoui@gmail.com>
2023-02-23 22:24:13 +0100

    added ham menu with some functionalities"

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function func() {
   console.log("hello there");
 }
+
+let hamBtn = document.getElementById("hamBtn");
+let hamMenu = document.getElementById("hamMenu");
+let hambtnfirst = document.getElementById("hambtnfirst");
+let hambtnsecond = document.getElementById("hambtnsecond");
+let hambtnthird = document.getElementById("hambtnthird");
+
+hamBtn.addEventListener("click", () => {
+  hamMenu.classList.toggle("hide");
+  hamMenu.classList.toggle("modal-bg");
+  hambtnfirst.classList.toggle("first-toggle");
+  hambtnsecond.classList.toggle("second-toggle");
+  hambtnthird.classList.toggle("third-toggle");
+});
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("hide");
+  hamMenu.classList.toggle("modal-bg");
+  hambtnfirst.classList.toggle("first-toggle");
+  hambtnsecond.classList.toggle("second-toggle");
+  hambtnthird.classList.toggle("third-toggle");
+});
